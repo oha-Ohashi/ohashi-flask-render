5d7bda3776d221faf2df16f8f1623705abebf629
GitKraken on MALKIA3 <matomatox1@gmail.com>
2023-04-01 04:50:42 +0900

    sheet kamasu

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, send_from_directory, abort, render_template
+from flask import Flask, request, send_from_directory, abort, render_template, make_response
+import requests
 import os
 
 app = Flask(__name__)
@@ -8,8 +9,38 @@ app = Flask(__name__)
 def raw_data(filename):
     if filename == "data.txt":
         return send_from_directory(os.getcwd(), filename, as_attachment=True)
+    elif filename == "sheet.txt":
+        url = "https://script.google.com/macros/s/AKfycbz6xvtbSFZ30LsjnSQPchgiRiTlMkouUr55P_ukmLECFICabPFOme8_chpA4EoeND1Zsw/exec?mode=view"
+        response = requests.get(url)
+        print(response.text)
+
+        httpres = make_response(response.text, 200)
+        httpres.mimetype = "text/plain"
+        return httpres
     else:
         abort(404)
+
+# 生データを取得するエンドポイント
+@app.route('/updatesheet')
+def update_sheet():
+    username = request.args.get('displayName')
+    number = request.args.get('rankCode')
+
+    if not username or not number or not number.isdigit() or int(number) not in range(10):
+        return "Invalid input.", 400
+
+    url = "https://script.google.com/macros/s/AKfycbz6xvtbSFZ30LsjnSQPchgiRiTlMkouUr55P_ukmLECFICabPFOme8_chpA4EoeND1Zsw/exec"
+    response = requests.post(url, data={
+        'displayName': username,
+        'rankCode': number
+    })
+
+    if (response.status_code == 200):
+        res = "good job!<br>"
+        res += "<a href='./update-html'>get back</a>"
+        return res
+    else: 
+        return "something went wrong"
 
 # data.txtを更新するエンドポイント
 @app.route('/update')
